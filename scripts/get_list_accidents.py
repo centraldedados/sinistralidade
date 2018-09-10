a3bdff9caf9c84916a15779fdf6b29ba2436eb13
Luís Fonseca <luispfonseca@gmail.com>
2018-09-10 17:30:17 +0100

    Remove o tratamento dos csv para casos como de 2012, dado que não resolvem todos os problemas com o csv. tabula-py fica a importar tabelas como possível; correcções serão feitas no fim

--- scripts/get_list_accidents.py
+++ scripts/get_list_accidents.py
@@ -74,60 +74,10 @@ def merge_csv(pdf_input):
     for file in allFiles:
         os.remove(file)
 
-
-def emptycols(file):
-
-    with open(file, newline="") as f:
-        reader = csv.reader(f)
-        try:
-            row1 = next(reader)
-        except StopIteration:
-            row1 = ""
-
-    return [idx for idx, item in enumerate(row1) if item == ""]
-
-
-def fix_empty_columns(pdf_input):
-    csv_filename = os.path.splitext(pdf_input.replace(" ", "_"))[0] + ".csv"
-
-    # fix empty column headers when converting some files
-    # ex: "",Concelho,Datahora,M,FG,,Via,Km,Natureza
-    # Get index of columns with empty header to be merged
-    # Merge columns recursively when empty
-    while emptycols(csv_filename):
-
-        emptycoltomerge = emptycols(csv_filename)[0]
-
-        temp_filename = csv_filename + "_temp"
-        os.rename(csv_filename, temp_filename)
-
-        with open(temp_filename) as f:
-            reader = csv.reader(f)
-            with open(csv_filename, "w", newline="") as g:
-                writer = csv.writer(g)
-                for row in reader:
-                    try:
-                        new_row = (
-                            row[0:emptycoltomerge]
-                            + [
-                                "".join(
-                                    [row[emptycoltomerge], row[emptycoltomerge + 1]]
-                                )
-                            ]
-                            + row[emptycoltomerge + 2 :]
-                        )
-                    # some rows are missing a last empty column in these files
-                    except IndexError:
-                        new_row = row[0:emptycoltomerge]
-                    writer.writerow(new_row)
-        os.remove(temp_filename)
-
-
 def get_list_accidents(pdf_input):
     convert_pdfs(pdf_input)
     filter_csv(pdf_input)
     merge_csv(pdf_input)
-    fix_empty_columns(pdf_input)
 
 
 ## Uncomment these to call this function from a command line
