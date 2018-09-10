95e0a2f0328b72b419efe3204c0854bd95cfa107
Luís Fonseca <luispfonseca@gmail.com>
2018-09-10 12:35:46 +0100

    Acrescenta script para processar todos os ficheiros pdf com lista de acidentes

--- scripts/get_list_accidents.py
+++ scripts/get_list_accidents.py
@@ -7,50 +7,66 @@ import shutil
 import csv
 import glob
 
-assert len(sys.argv[1:]) == 1
-pdf_filename = str(sys.argv[1])
 
 def get_num_pages_pdf(file):
-	pdfFileObj = open(file, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	return pdfReader.numPages
+    pdfFileObj = open(file, "rb")
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    return pdfReader.numPages
 
-def convert_pdfs():
-	for page in range(1,get_num_pages_pdf(pdf_filename)+1):
-		print(page)
-		csv_filename = str(pdf_filename) + '_p' + str(page) + '.csv'
-		tabula.convert_into(pdf_filename, csv_filename, output_format="csv", encoding='latin-1', pages=page, multiple_tables = True, lattice = True)
 
-def filter_csv():
-	# keep only the files with the list of accidents
-	# we will filter for those by looking for "Natureza" in the header of the csv
-	for file in glob.glob(pdf_filename + "_p*.csv"):
-		#print(file)
-		with open(file, newline='') as f:
-			reader = csv.reader(f)
-			row1 = next(reader)
-		f.close()
-		if "Natureza" not in row1:
-			os.remove(file)
+def convert_pdfs(pdf_input):
+    for page in range(1, get_num_pages_pdf(pdf_input) + 1):
+        print(page)
+        csv_filename = str(pdf_input) + "_p" + str(page) + ".csv"
+        tabula.convert_into(
+            pdf_input,
+            csv_filename,
+            output_format="csv",
+            encoding="latin-1",
+            pages=page,
+            multiple_tables=True,
+            lattice=True,
+        )
 
-def merge_csv():
-	allFiles = glob.glob(pdf_filename + "_p*.csv")
-	print(allFiles)
-	with open(os.path.join(pdf_filename + ".csv"), 'wb') as outfile:
-	    for i, fname in enumerate(allFiles):
-	        with open(fname, 'rb') as infile:
-	            if i != 0:
-	                infile.readline()  # Throw away header on all but first file
-	            # Block copy rest of file from input to output without parsing
-	            shutil.copyfileobj(infile, outfile)
-	            print(fname + " has been imported.")
-	os.rename(pdf_filename + ".csv", os.path.splitext(pdf_filename.replace(" ", "_"))[0] + ".csv")
-	for file in allFiles:
-		os.remove(file)
 
-def main():
-	convert_pdfs()
-	filter_csv()
-	merge_csv()
+def filter_csv(pdf_input):
+    # keep only the files with the list of accidents
+    # we will filter for those by looking for "Natureza" in the header of the csv
+    for file in glob.glob(pdf_input + "_p*.csv"):
+        # print(file)
+        with open(file, newline="") as f:
+            reader = csv.reader(f)
+            row1 = next(reader)
+        f.close()
+        if "Natureza" not in row1:
+            os.remove(file)
 
-main()
+
+def merge_csv(pdf_input):
+    allFiles = glob.glob(pdf_input + "_p*.csv")
+    print(allFiles)
+    with open(os.path.join(pdf_input + ".csv"), "wb") as outfile:
+        for i, fname in enumerate(allFiles):
+            with open(fname, "rb") as infile:
+                if i != 0:
+                    infile.readline()  # Throw away header on all but first file
+                # Block copy rest of file from input to output without parsing
+                shutil.copyfileobj(infile, outfile)
+                print(fname + " has been imported.")
+    os.rename(
+        pdf_input + ".csv", os.path.splitext(pdf_input.replace(" ", "_"))[0] + ".csv"
+    )
+    for file in allFiles:
+        os.remove(file)
+
+
+def get_list_accidents(pdf_input):
+    convert_pdfs(pdf_input)
+    filter_csv(pdf_input)
+    merge_csv(pdf_input)
+
+
+## Uncomment these to call this function from a command line
+# assert len(sys.argv[1:]) == 1, "Only one argument allowed: the path to the pdf"
+# pdf_filename = str(sys.argv[1])
+# get_list_accidents(pdf_filename)
---
+++ scripts/process_all_pdf.py
@@ -0,0 +1,68 @@
+from get_list_accidents import get_list_accidents
+import os
+
+# create folder to store data in csv
+path_csv = os.path.join("..", "dados")
+if not os.path.exists(path_csv):
+    os.makedirs(path_csv)
+
+# list districts
+distritos = [
+    "Aveiro",
+    "Beja",
+    "Braga",
+    "Bragança",
+    "Castelo",
+    "Coimbra",
+    "Évora",
+    "Faro",
+    "Guarda",
+    "Leiria",
+    "Lisboa",
+    "Portalegre",
+    "Porto",
+    "Santarém",
+    "Setúbal",
+    "Viana",
+    "Vila",
+    "Viseu",
+]
+
+# define function to process all the districts in a given year
+def process_all_pdfs_in_folder(year):
+
+    path_pdfs = os.path.join("..", "pdfs", str(year))
+
+    # get list of all pdf files inside the folder
+    list_pdfs = [f for f in os.listdir(path_pdfs) if f.endswith(".pdf")]
+
+    print("List of pdf files in the folder:", list_pdfs)
+
+    # keep only the pdfs that start with a district name
+    final_list = [
+        item for item in list_pdfs for distrito in distritos if distrito in item
+    ]
+    final_list = list(
+        sorted(set(final_list))
+    )  # drops duplicates from the list, caused by Braga and Bragança
+
+    print("Final list of files:", final_list)
+
+    # for each file, extract the list of accidents and move it to the data folder
+    for file in final_list:
+
+        # convert list of accidents to csv
+        path_pdf_file = os.path.join(path_pdfs, file)
+        get_list_accidents(path_pdf_file)
+        csv_filename = os.path.splitext(file.replace(" ", "_"))[0] + ".csv"
+
+        # move the file
+        os.rename(
+            os.path.join(path_pdfs, csv_filename), os.path.join(path_csv, csv_filename)
+        )
+
+
+# loop through all the years in the data
+for year in range(2013, 2019):
+    print("Processing year", year)
+    process_all_pdfs_in_folder(year)
